94e263128ddd6bb67b8b9481770782a7d9b602d0
m305-4\w401 <djsiao123@jdio.com>
2018-05-17 18:00:35 +0800

    第一次修改

---
+++ ToDoList/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //產生一個方塊
+            List List = new List();
+
+            //放進TodoStack
+            TodoStack.Children.Add(List);
+        }
+
+        private void SaveBtn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            List<string> all = new List<string>();
+            //取得每一個 item 的文字
+            foreach ( List item in TodoStack.Children)
+            {
+                all.Add(item.GetTaskName());
+            }
+
+            //寫入檔案
+            System.IO.File.WriteAllLines(@"c:\tododata.txt", all);
+        }
+    }
+}
